d62ce6584c2b8458109b0b36045676212e52fc17
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-03-18 14:41:12 +0300

    Обнавление работы

--- plugins/wp-e-commerce/wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
+++
@@ -1,6 +0,0 @@
-;(function($) {
-	$( '.wpsc-field-textfield' ).FloatLabel();
-	$( '.wpsc-field-select_region' ).FloatLabel();
-	$( '.wpsc-field-select_country' ).FloatLabel();
-	$( '.wpsc-field-password' ).FloatLabel();
-})(jQuery);
